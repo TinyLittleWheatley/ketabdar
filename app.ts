035f83d4b3b983739bdb11bdb34358b3bafabbc6
Mohammad <1380mkkarami@gmail.com>
2022-02-08 13:04:25 +0330

    Set webhook

--- app.ts
+++ app.ts
@@ -1,13 +1,15 @@
 import { Telegraf } from 'telegraf'
 import { BaseContoller } from './Controllers'
 import middlewares, { CustomContext } from './Middlewares'
+import fs from 'fs' 
 
 const bot = new Telegraf<CustomContext>(String(process.env.BOT_TOKEN))
 bot.use(...middlewares)
 new BaseContoller(bot)
-bot.launch()
 
-
+// Set telegram webhook
+bot.telegram.setWebhook('http://NnbyO-40499.portmap.host:40499')
+bot.launch({webhook:{port:3000}})
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
